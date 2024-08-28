1986fc044528c3fc7af8b100f06bf9d9587496bb
Laxen <maximiliam.froste@outlook.com>
2024-08-28 16:16:08 +0200

    SDL 2 funkar nu.

---
+++ SDL Rendering/src/Main.cpp
@@ -0,0 +1,12 @@
+#define SDL_MAIN_HANDLED
+
+#include <SDL.h>
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    std::cout << "SDL Works!" << std::endl;
+    std::cin.get();
+
+    return 0;
+}
