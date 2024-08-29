3fc2ecafbfaa0268f3e555ae52c134e66ff69110
Laxen <maximiliam.froste@outlook.com>
2024-08-29 19:25:10 +0200

    Mosue input

--- SDL Rendering/src/Main.cpp
+++ SDL Rendering/src/Main.cpp
@@ -2,66 +2,14 @@
 #include <windows.h>
 #include <SDL.h>
 #include <iostream>
-
-int mouse()
-{
-    HANDLE hIn;
-    HANDLE hOut;
-    COORD KeyWhere;
-    COORD MouseWhere;
-    COORD EndWhere;
-    bool Continue = TRUE;
-    int KeyEvents = 0;
-    int MouseEvents = 0;
-    INPUT_RECORD InRec;
-    DWORD NumRead;
-
-    hIn = GetStdHandle(STD_INPUT_HANDLE);
-    hOut = GetStdHandle(STD_OUTPUT_HANDLE);
-
-    std::cout << "Key Events   : " << std::endl;
-    std::cout << "Mouse Events : " << std::flush;
-
-    KeyWhere.X = 15;
-    KeyWhere.Y = 0;
-    MouseWhere.X = 15;
-    MouseWhere.Y = 1;
-    EndWhere.X = 0;
-    EndWhere.Y = 3;
-
-    while (Continue)
-    {
-        ReadConsoleInput(hIn,&InRec,1, &NumRead);
-
-        switch (InRec.EventType)
-        {
-        case KEY_EVENT:
-            ++KeyEvents;
-            SetConsoleCursorPosition(hOut,KeyWhere);
-            std::cout << KeyEvents << flush;
-            if (InRec.Event.KeyEvent.uChar.AsciiChar == 'x')
-            {
-                SetConsoleCursorPosition(hOut,EndWhere);
-                std::cout << "Exiting..." << std::endl;
-                Continue = FALSE;
-            }
-            break;
-
-        case MOUSE_EVENT:
-            ++MouseEvents;
-            SetConsoleCursorPosition(hOut, MouseWhere);
-            std::cout << MouseEvents << flush;
-            break;
-        }
-    }
-}
+#include <thread>
 
 int gravity()
 {
-    int grid[70][70];
-    for (int x = 0; x < 70; x++)
+    int grid[640-1][640-1];
+    for (int x = 0; x < 640; x++)
     {
-        for (int y = 0; y < 70; y++)
+        for (int y = 0; y < 640; y++)
         {
             grid[x][y] = 0;
             std::cout << "x: " << x << " , " << "y: "<<  y << std::endl;
@@ -71,28 +19,63 @@ int gravity()
 }
 
 int main(int argc, char* argv[])
-{
+{   
     if (SDL_Init(SDL_INIT_VIDEO != 0))
         std::cout << "Error: " << SDL_GetError();
     else
     {
-        SDL_Window* window
-        SDL_Renderer* renderer
-        SDL_CreateWindowAndRenderer(70*10, 70*10, 0, &window, &renderer);
+
+        SDL_Window* window;
+        SDL_Renderer* renderer;
+        SDL_CreateWindowAndRenderer(64*10, 64*10, 0, &window, &renderer);
         SDL_RenderSetScale(renderer, 10, 10);
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-        SDL_RenderDrawPoint(renderer, 70/2, 70/2);
+        SDL_RenderDrawPoint(renderer, 64/2, 64/2);
         SDL_RenderPresent(renderer);
-
-        gravity(    );
 
         while (true)
         {
             SDL_Event event;
-            SDL_PollEvent(&event);
-
-            if (event.type == SDL_QUIT)
-                break;
+            if (SDL_PollEvent(&event));
+            {
+                if (event.type == SDL_QUIT)
+                    break;
+                if (event.type == SDL_MOUSEMOTION)
+                {
+                    int x,y;
+                    SDL_GetMouseState( &x, &y);
+                    std::cout << x <<" : " << y << std::endl;
+                }
+                if (event.type == SDL_MOUSEBUTTONDOWN)
+                {
+                    if(SDL_BUTTON_LEFT == event.button.button)
+                        {std::cout << "LMB DOWN" << std::endl; }
+                    if(SDL_BUTTON_RIGHT == event.button.button)
+                        {std::cout << "RMB DOWN" << std::endl; }
+                    if(SDL_BUTTON_MIDDLE == event.button.button)
+                        {std::cout << "MMB DOWN" << std::endl; }
+                }
+                if (event.type == SDL_MOUSEBUTTONUP)
+                {
+                    if(SDL_BUTTON_LEFT == event.button.button)
+                        {std::cout << "LMB UP" << std::endl; }
+                    if(SDL_BUTTON_RIGHT == event.button.button)
+                        {std::cout << "RMB UP" << std::endl; }
+                    if(SDL_BUTTON_MIDDLE == event.button.button)
+                        {std::cout << "MMB UP" << std::endl; }
+                }
+            } 
+    int grid[640-1][640-1];
+    for (int x = 0; x < 640; x++)
+    {
+        for (int y = 0; y < 640; y++)
+        {
+            grid[x][y] = 0;
+            //std::cout << "x: " << x << " , " << "y: "<<  y << std::endl;
+        }
+    }
+    std::cout << "List completed" << std::endl;
+    return 0;
         }
 
         SDL_DestroyWindow(window);
