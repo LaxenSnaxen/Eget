19728a22cdca712cfb458c8f865a43ca7a05daeb
Laxen <maximiliam.froste@outlook.com>
2024-08-29 09:00:19 +0200

    SDL funkar nu, genuint.

--- SDL Rendering/src/Main.cpp
+++ SDL Rendering/src/Main.cpp
@@ -6,7 +6,26 @@
 
 int main(int argc, char* argv[])
 {
-    std::cout << "SDL Works :)" << std::endl;
-    std::cin.get();
+    if (SDL_Init(SDL_INIT_VIDEO != 0))
+        std::cout << "Error: " << SDL_GetError();
+    else
+    {
+        SDL_Window* window;
+        window = SDL_CreateWindow("Test Window", 400, 300, 800, 600, SDL_WINDOW_SHOWN);
+
+        while (true)
+        {
+            SDL_Event event;
+            SDL_PollEvent(&event);
+
+            if (event.type == SDL_QUIT)
+                break;
+        }
+
+        SDL_DestroyWindow(window);
+    }
+
+    SDL_Quit();
+
     return 0;
 }
