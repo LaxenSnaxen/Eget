c23e3aa5c4c4a88db4838d5ecf0357cac501de5d
Laxen <maximiliam.froste@outlook.com>
2024-09-09 14:27:05 +0200

    Det fungerar, lite härta, men det fungerar.

--- SDL Rendering/src/Main.cpp
+++ SDL Rendering/src/Main.cpp
@@ -3,51 +3,21 @@
 #include <SDL.h>
 #include <iostream>
 #include <thread>
+#include <cmath>
 
-/*void mouse()
-            {
-                    while (true)
-                    {   
-                        SDL_Event event;
-                        if (SDL_PollEvent(&event));
-                        {
-                            if (event.type == SDL_QUIT)
-                                break;
-                            if (event.type == SDL_MOUSEMOTION)
-                            {
-                                int x,y;
-                                SDL_GetMouseState( &x, &y);
-                                std::cout << x <<" : " << y << std::endl;
-                            }
-                            if (event.type == SDL_MOUSEBUTTONDOWN)
-                            {
-                                if(SDL_BUTTON_LEFT == event.button.button)
-                                    {std::cout << "LMB DOWN" << std::endl; }
-                                if(SDL_BUTTON_RIGHT == event.button.button)
-                                    {std::cout << "RMB DOWN" << std::endl; }
-                                if(SDL_BUTTON_MIDDLE == event.button.button)
-                                    {std::cout << "MMB DOWN" << std::endl; }
-                            }
-                            if (event.type == SDL_MOUSEBUTTONUP)
-                            {
-                                if(SDL_BUTTON_LEFT == event.button.button)
-                                    {std::cout << "LMB UP" << std::endl; }
-                                if(SDL_BUTTON_RIGHT == event.button.button)
-                                    {std::cout << "RMB UP" << std::endl; }
-                                if(SDL_BUTTON_MIDDLE == event.button.button)
-                                    {std::cout << "MMB UP" << std::endl; }
-                            }
-                        }
-                    }
+template<typename T>
 
-            }
-*/
-
-void workFunc(int times)
+int removeOldPointCreatNew(T renderer, int x, int y, int nx, int ny, int** grid)
 {
-	while(times) {
-		std::cout << "YEAH"<< std::endl;
-	}
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderDrawPoint(renderer, x, y);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderDrawPoint(renderer, nx, ny);
+    SDL_RenderPresent(renderer);
+    grid [x] [y] = 0;
+    grid [nx] [ny] = 1;
+    SDL_Delay(10);
+    return 0;
 }
 
 int main(int argc, char* argv[])
@@ -62,24 +32,91 @@ int main(int argc, char* argv[])
         SDL_CreateWindowAndRenderer(64*10, 64*10, 0, &window, &renderer);
         SDL_RenderSetScale(renderer, 10, 10);
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-        SDL_RenderDrawPoint(renderer, 64/2, 64/2);
+        //SDL_RenderDrawPoint(renderer, 64/2, 64/2);
         SDL_RenderPresent(renderer);
-
-    int grid[64-1][64-1];
-    for (int x = 0; x < 64; x++)
-    {
-        std::cout << "x: " << x << std::endl;
-        for (int y = 0; y < 64; y++)
-        {
-            std::cout << "y: " << y << std::endl;
-            grid[x][y] = 0;
         
+        int** grid = new int*[64];
+        for (int x = 0; x < 64; x++)
+            grid[x] = new int[64];
+
+
+        while (true)
+        {   
+            bool lmbDown = false;
+            
+            SDL_Event event;
+            if (SDL_PollEvent(&event));
+            {
+                if (event.type == SDL_QUIT)
+                    break;
+                /*if (event.type == SDL_MOUSEMOTION)
+                {
+                    int x,y;
+                    SDL_GetMouseState( &x, &y);
+                    std::cout << x <<" : " << y << std::endl;
+                }*/
+                if (event.type == SDL_MOUSEBUTTONDOWN)
+                {
+                    if(SDL_BUTTON_LEFT == event.button.button)
+                        {std::cout << "LMB DOWN" << std::endl;
+                        lmbDown = true;
+                        int x,y;
+                        SDL_GetMouseState(&x,&y);
+                        x =  round(x/10);
+                        y =  round(y/10);
+                        std::cout << x << ":" << y << std::endl;
+                        grid[x][y] = 1;
+                        std::cout << grid[x][y] << std::endl;
+                        SDL_RenderDrawPoint(renderer, x, y);
+                        SDL_RenderPresent(renderer);
+                        if (event.type == SDL_MOUSEMOTION)
+                        {}}
+                    if(SDL_BUTTON_RIGHT == event.button.button)
+                        {std::cout << "RMB DOWN" << std::endl; }
+                    if(SDL_BUTTON_MIDDLE == event.button.button)
+                        {std::cout << "MMB DOWN" << std::endl; }
+                }
+                if (event.type == SDL_MOUSEBUTTONUP)
+                {
+                    if(SDL_BUTTON_LEFT == event.button.button)
+                        {std::cout << "LMB UP" << std::endl; 
+                        lmbDown = false;}
+                    if(SDL_BUTTON_RIGHT == event.button.button)
+                        {std::cout << "RMB UP" << std::endl; }
+                    if(SDL_BUTTON_MIDDLE == event.button.button)
+                        {std::cout << "MMB UP" << std::endl; }
+                }
+            }
+
+            for (int x = 0; x < 64; x++)
+            {
+            for (int y = 0; y < 64; y++)
+                {
+                if (grid[x][y] == 1 && y != 63)
+                    {
+                    if (grid[x][y+1] == 0)
+                        {
+                        int nx = x;
+                        int ny = y + 1;
+                        removeOldPointCreatNew(renderer, x, y, nx, ny, grid);
+                        }
+                    else if (grid[x+1][y+1] == 0)
+                        {
+                        int nx = x + 1;
+                        int ny = y + 1;
+                        removeOldPointCreatNew(renderer, x, y, nx, ny, grid);
+                        }
+                    else if (grid[x+1][y+1] == 0)
+                        {
+                        int nx = x - 1;
+                        int ny = y + 1;
+                        removeOldPointCreatNew(renderer, x, y, nx, ny, grid);
+                        }
+                    }
+                }
+            }
         }
-        std::cout << "List completed" << std::endl;
-    }
-    std::cout << "Full List completed" << std::endl;
-    std::thread mouseInput(workFunc(300));
-    mouseInput.join();
+
     SDL_Delay(100000);
     
 
--- SDL Rendering/src/cell.hpp
+++ SDL Rendering/src/cell.hpp
@@ -1,7 +1,13 @@
 #pragma once
 
-Struct cell
+typedef struct SDL_UserEvent
 {
-    int id;
-    bool occupied;
-}
+    Uint32 type;        /**< SDL_USEREVENT through SDL_LASTEVENT-1 */
+    Uint32 timestamp;   /**< In milliseconds, populated using SDL_GetTicks() */
+    Uint32 windowID;    /**< The associated window if any */
+    Sint32 code;        /**< User defined event code */
+    void *data1;        /**< User defined data pointer */
+    void *data2;        /**< User defined data pointer */
+
+    
+} SDL_UserEvent;
