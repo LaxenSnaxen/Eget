be368dd8ee0f231b7a71f867bf7cc3676d5a9604
Laxen <maximiliam.froste@outlook.com>
2024-10-04 15:09:14 +0200

    Vidare framgångar vid användning av openGL

--- Mandelbrotset/src/main.cpp
+++ Mandelbrotset/src/main.cpp
@@ -5,6 +5,27 @@
 
 const int WIDTH =   800;
 const int HEIGHT =  600;
+float vertices [] = 
+{
+    -0.5f, -0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    0.0f, 0.5f, 0.0f
+}
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+    {
+        glViewport(0, 0, width, height);
+    }
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
 
 int main()
 {
@@ -23,11 +44,35 @@ int main()
     }
     glfwMakeContextCurrent(window);
     
-if  (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-{
-    std::cout << "Failed to initialize GLAD" << std::endl;
-    return -1;
-}
+    if  (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
 
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices)
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        // Input.
+        processInput(window);
+
+        // Rendering commands here:
+
+
+        // Check and call events and swap the buffers.
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+    }
+
+
+    glfwTerminate();
     return 0;
 }
